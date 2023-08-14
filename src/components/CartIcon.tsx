19ad9f7924b62d40536d26b1e072e490b578c466
Shr4528 <Shr4528@gmail.com>
2023-08-14 12:04:24 -0500

    mobilemenu

--- src/components/CartIcon.tsx
+++ src/components/CartIcon.tsx
@@ -2,7 +2,15 @@ import Link from 'next/link';
 import React from 'react';
 
 const CartIcon = () => {
-  return <Link href='/cart'>CartIcon</Link>;
+  return (
+    <Link href='/cart' className='flex items-center gap-4'>
+      CartIcon
+      <div className='relative w-9 h-9  md:w-5 md:h-5'>
+        <Image src='/cart.png' alt='cart' fill />
+      </div>
+      <span>Cart</span>
+    </Link>
+  );
 };
 
 export default CartIcon;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,12 +5,17 @@ import Link from 'next/link';
 const Navbar = () => {
   return (
     <div className='h-12 text-red-500 p-4 flex items-center cursor-pointer justify-between border-b-2 border-b-red-500 uppercase'>
+      <div className='hidden md:flex gap-4'>
+        <Link href='/'>HomePage</Link>
+        <Link href='/menu'>Menu</Link>
+        <Link href='/contact'>Contact</Link>
+      </div>
       {/* LOGO */}
       <div className='text-xl'>
         <Link href='/'>Massimo</Link>
       </div>
       {/* MOBILE MENU */}
-      <div>
+      <div className='md:hidden'>
         <Menu />
       </div>
     </div>
