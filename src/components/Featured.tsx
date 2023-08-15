0a9df303a802e78f0599b170ca224bf05f812210
Shr4528 <Shr4528@gmail.com>
2023-08-15 10:14:57 -0500

    featured

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import Image from 'next/image';
 
 const Featured = () => {
-  return <div>Featured</div>;
+  return (
+    <div className=''>
+      {/* wrapper */}
+      <div className=''>
+        {/* single item */}
+        <div className=''>
+          {/* image */}
+          <div className='relative'>
+            <Image src='' alt='' fill />
+          </div>
+          {/* text container */}
+          <div className=''>
+            <h1 className=''>Title</h1>
+            <p className=''>Description</p>
+            <span>123</span>
+            <button className=''>Add to Cart</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Featured;
