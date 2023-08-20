3984b53118f2028b2cdbec0fc073f60d89ab1d1d
Shr4528 <Shr4528@gmail.com>
2023-08-20 18:58:19 -0500

    price

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import Image from 'next/image';
+import { singleProduct } from '@/data';
+import Price from '@/components/Price';
 
 const SingleProductPage = () => {
-  return <div>ProductPage</div>;
+  return (
+    <div className='p-4 lg:px-20 xl:px-40 h-screen flex flex-col justify-around text-red-400 md:flex-row'>
+      {/* image container */}
+      {singleProduct.img && (
+        <div className='relative'>
+          <Image
+            src={singleProduct.img}
+            alt=''
+            fill
+            className='object-contain'
+          />
+        </div>
+      )}
+      {/* text container */}
+      <div>
+        <h1>{singleProduct.title}</h1>
+        <p>{singleProduct.desc}</p>
+        <Price
+          price={singleProduct.price}
+          id={singleProduct.id}
+          options={singleProduct.options}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default SingleProductPage;
