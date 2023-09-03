de05c5f8cbd1084dcca5da63aeec566c738472d6
Shr4528 <Shr4528@gmail.com>
2023-09-03 11:58:27 -0500

    uiend

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -3,17 +3,39 @@ import React from 'react';
 const OrderPage = () => {
   return (
     <div className='p-4 lg:px-20 xl:px-40'>
-      <table className=''>
+      <table className='w-full border-separate border-spacing-4'>
         <thead>
-          <tr>
-            <th>Order</th>
+          <tr className='text-left'>
+            <th className='hidden md:block'>Order</th>
             <th>Date</th>
             <th>Price</th>
-            <th>Products</th>
+            <th className='hidden md:block'>Products</th>
             <th>Status</th>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+          <tr className='text-sm md:text-base odd:bg-red-50'>
+            <td className='hidden md:block py-6 px-1'>223432142413135</td>
+            <td className='py-6 px-1'>9.4.2023</td>
+            <td className='py-6 px-1'>Price</td>
+            <td className='hidden md:block  py-6 px-1'>Big Burger</td>
+            <td className='py-6 px-1'>On the way</td>
+          </tr>
+          <tr className='text-sm md:text-base odd:bg-slate-400'>
+            <td className='hidden md:block py-6 px-1'>223432142413135</td>
+            <td className='py-6 px-1'>9.4.2023</td>
+            <td className='py-6 px-1'>Price</td>
+            <td className='hidden md:block  py-6 px-1'>Big Burger</td>
+            <td className='py-6 px-1'>On the way</td>
+          </tr>
+          <tr className='text-sm md:text-base odd:bg-red-50'>
+            <td className='hidden md:block py-6 px-1'>223432142413135</td>
+            <td className='py-6 px-1'>9.4.2023</td>
+            <td className='py-6 px-1'>Price</td>
+            <td className='hidden md:block  py-6 px-1'>Big Burger</td>
+            <td className='py-6 px-1'>On the way</td>
+          </tr>
+        </tbody>
       </table>
     </div>
   );
