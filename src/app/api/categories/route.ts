5df3d689f02eec856834362906433d7e663e6749
Shr4528 <Shr4528@gmail.com>
2023-09-03 17:19:35 -0500

    docker

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,5 +1,22 @@
+import { PrismaClient } from '@prisma/client';
 import { NextResponse } from 'next/server';
 
-export const GET = () => {
+const prisma = new PrismaClient();
+
+// fetch all categories
+export const GET = async () => {
+  try {
+    const categories = await prisma.category.findMany();
+    return new NextResponse(JSON.stringify({ categories }), { status: 200 });
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: 'SOMETHING went wrong' }),
+      { status: 500 }
+    );
+  }
+};
+
+export const POST = () => {
   return new NextResponse('Hello', { status: 200 });
 };
