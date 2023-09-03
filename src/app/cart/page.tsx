a185a953d9d4455a5c2d989688d0999ca379ca5a
Shr4528 <Shr4528@gmail.com>
2023-09-03 11:15:11 -0500

    orders

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -3,9 +3,19 @@ import React from 'react';
 
 const CartPage = () => {
   return (
-    <div className='h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col text-red-500'>
+    <div className='h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col text-red-500 lg:flex-row'>
       {/* products container */}
-      <div className='h-1/2 p-4 flex flex-col justify-center overflow-scroll'>
+      <div className='h-1/2 p-4 flex flex-col justify-center overflow-scroll lg:h-full lg:w-2/3 2xl:w-1/2 lg:px-20 xl:px-40'>
+        {/* single item */}
+        <div className='flex items-center justify-between mb-4 '>
+          <Image src='/temporary/p1.png' alt='' width={100} height={100} />
+          <div className=''>
+            <h1 className='uppercase text-xl font-bold'>sicilian</h1>
+            <span>Large</span>
+          </div>
+          <h2>$79.90</h2>
+          <span className='cursor-pointer'>X</span>
+        </div>
         {/* single item */}
         <div className='flex items-center justify-between mb-4 '>
           <Image src='/temporary/p1.png' alt='' width={100} height={100} />
@@ -28,14 +38,26 @@ const CartPage = () => {
         </div>
       </div>
       {/* payment container */}
-      <div className='h-1/2 p-4 bg-fuchsia-50'>
-        <div className=''>
+      <div className='h-1/2 p-4 bg-fuchsia-50 flex flex-col gap-4 justify-center lg:h-full lg:w-1/3 2xl:w-1/2 2xl:gap-6'>
+        <div className='flex justify-between '>
           <span>Subtotal</span>
           <span> $88.70</span>
         </div>
-        <hr className='' />
-        <button className='bg-red-500 text-white p-3 rounded-md w-1/2'>
-          CHEKOUT
+        <div className='flex justify-between '>
+          <span>Service Cost</span>
+          <span> $0.00</span>
+        </div>
+        <div className='flex justify-between '>
+          <span>Delivery Cost</span>
+          <span className='text-green-500'>Free</span>
+        </div>
+        <hr className='my-2' />
+        <div className='flex justify-between '>
+          <span>TOTAL Number</span>
+          <span className='font-bold'>$91.90</span>
+        </div>
+        <button className='bg-red-500 text-white p-3 rounded-md w-1/2 self-end'>
+          CHECKOUT
         </button>
       </div>
     </div>
--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
 const OrderPage = () => {
-  return <div>OrderPage</div>;
+  return (
+    <div className='p-4 lg:px-20 xl:px-40'>
+      <table className=''>
+        <thead>
+          <tr>
+            <th>Order</th>
+            <th>Date</th>
+            <th>Price</th>
+            <th>Products</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
+    </div>
+  );
 };
 
 export default OrderPage;
