d306d029d3bdd3c87b0479f324d5890600e1cd66
Shr4528 <Shr4528@gmail.com>
2023-09-15 16:17:46 -0500

    order

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,10 +1,11 @@
 'use client';
 
 import { OrderType } from '@/types/types';
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { useSession } from 'next-auth/react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import { toast } from 'react-toastify';
 
 const OrdersPage = () => {
   const { data: session, status } = useSession();
@@ -20,14 +21,33 @@ const OrdersPage = () => {
       fetch('http://localhost:3000/api/orders').then((res) => res.json()),
   });
 
-  if (isLoading || status === 'loading') return 'Loading...';
+  const queryClient = useQueryClient();
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>, id: string) => {
+  const mutation = useMutation({
+    mutationFn: ({ id, status }: { id: string; status: string }) => {
+      return fetch(`http://localhost:3000/api/orders/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(status),
+      });
+    },
+    onSuccess() {
+      queryClient.invalidateQueries({ queryKey: ['orders'] });
+    },
+  });
+  const handleUpdate = (e: React.FormEvent<HTMLFormElement>, id: string) => {
     e.preventDefault();
     const form = e.target as HTMLFormElement;
     const input = form.elements[0] as HTMLInputElement;
     const status = input.value;
+
+    mutation.mutate({ id, status });
+    toast.success('The order status has been changed!');
   };
+
+  if (isLoading || status === 'loading') return 'Loading...';
 
   return (
     <div className='p-4 lg:px-20 xl:px-40'>
@@ -56,13 +76,13 @@ const OrdersPage = () => {
                 <td>
                   <form
                     className='flex items-center justify-center gap-4'
-                    onSubmit={(e) => handleSubmit(e, item.id)}>
+                    onSubmit={(e) => handleUpdate(e, item.id)}>
                     <input
                       placeholder={item.status}
-                      className='p-2 ring-1 ring-red-50 rounded-md'
+                      className='className="p-2 ring-1 ring-red-100 rounded-md'
                     />
                     <button className='bg-red-400 p-2 rounded-full'>
-                      <Image src='/edit.png' alt='' width={20} height={20} />
+                      <Image src='edit.png' alt='' width={20} height={20} />
                     </button>
                   </form>
                 </td>
